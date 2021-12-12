fed32b329f3fbf4f75f7819b056aafc59c618968
KirillRomadin <kirillromadin@gmail.com>
2021-12-12 15:06:27 +0300

    Logo
    Лого и Килл

--- Detectiv_BoBoBo/Assets/KillerScript.cs
+++ Detectiv_BoBoBo/Assets/KillerScript.cs
@@ -18,6 +18,7 @@ namespace Detectiv
         private float _time;
 
         SpriteRenderer[] spriteRenderers;
+        SpriteRenderer[] spriteRenderersKiller;
 
         void Start()
         {
@@ -26,11 +27,13 @@ namespace Detectiv
 
         void Update()
         {
-            _time = Time.timeScale;
+            _time += 1 * Time.deltaTime;
+
             if (_find)
             {
                 _foundNPC = GameObject.FindGameObjectsWithTag("NPC");
                 _foundKiller = GameObject.FindGameObjectWithTag("Killer");
+                spriteRenderersKiller = _foundKiller.GetComponentsInChildren<SpriteRenderer>();
                 _find = false;
             }
 
@@ -41,17 +44,37 @@ namespace Detectiv
             }
 
 
-
-            for (int i = 0; i < _foundNPC.Length; i++)
+            if(_time > 15)
             {
-                if (_foundNPC[i].transform.position.x > (_hero.transform.position.x + 30)
-                    || _foundNPC[i].transform.position.x < (_hero.transform.position.x - 30)
-                    || _foundNPC[i].transform.position.y > (_hero.transform.position.y + 17)
-                    || _foundNPC[i].transform.position.y < (_hero.transform.position.y - 17))
+                for (int i = 0; i < _foundNPC.Length; i++)
                 {
-                    Debug.Log("Yes");
+                    if (_foundNPC[i].transform.position.x > (_hero.transform.position.x + 30)
+                        || _foundNPC[i].transform.position.x < (_hero.transform.position.x - 30)
+                        || _foundNPC[i].transform.position.y > (_hero.transform.position.y + 17)
+                        || _foundNPC[i].transform.position.y < (_hero.transform.position.y - 17))
+                    {
+                        spriteRenderers = _foundNPC[i].GetComponentsInChildren<SpriteRenderer>();
+                        for (int j = 0; j < spriteRenderers.Length; j++)
+                        {
+                            for (int y = 0; y < spriteRenderersKiller.Length; y++)
+                            {
+                                if (spriteRenderers[j].gameObject.tag == spriteRenderersKiller[y].tag && _time > 15)
+                                {
+                                    Debug.Log("Hay");
+                                    var sp = _foundNPC[i].GetComponent<SpriteRenderer>();
+                                    sp.sprite = _sprite;
+                                    _time = 0;
+                                    _foundNPC[i].gameObject.tag = "Dezist";
+
+                                    break;
+                                }
+                            }
+                        }
+                    }
                 }
             }
+
+            Debug.Log(_time);
 
 
         }
